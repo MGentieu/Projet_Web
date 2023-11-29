742db9960b1bfd3626a93c2858592648d495783f
MGentieu <mgentieu02@gmail.com>
2023-11-30 00:14:37 +0100

    Update BDD_ECE_IN.sql
    Création de base mais incomplète du fichier SQL nécessaire à la construction de la base de données locale.

--- BDD_ECE_IN.sql
+++ BDD_ECE_IN.sql
@@ -1,43 +1,35 @@
-DROP DATABASE IF EXISTS BDCOMMANDES;
-CREATE DATABASE BDCOMMANDES ;
-USE BDCOMMANDES ;
-DROP TABLE IF EXISTS Fournisseurs ;
-DROP TABLE IF EXISTS Produits ;
-DROP TABLE IF EXISTS Commandes ;
-CREATE TABLE Fournisseurs (
-fno Numeric(6) NOT NULL primary key ,
-nom VARCHAR(25) NOT NULL ,
-adresse VARCHAR(25) ,
-ville VARCHAR(25) NOT NULL
+DROP DATABASE IF EXISTS ECE_IN;
+CREATE DATABASE ECE_IN ;
+USE ECE_IN ;
+DROP TABLE IF EXISTS Auteur ;
+DROP TABLE IF EXISTS Administrateur ;
+DROP TABLE IF EXISTS Image_de_fond ;
+
+CREATE TABLE Administrateur (
+email_admin VARCHAR(50) NOT NULL primary key ,
+mot_de_passe VARCHAR(30) NOT NULL ,
+nom VARCHAR(30) NOT NULL ,
+num_telephone Numeric(10) NOT NULL
 ) ;
-CREATE TABLE Produits (
-pno Numeric(6) NOT NULL primary key ,
-design VARCHAR(25) NOT NULL ,
-prix Numeric(6,2) NOT NULL ,
-poids Numeric(6,2) NOT NULL ,
-couleur VARCHAR(25) NOT NULL
+
+CREATE TABLE Image_de_fond (
+id_im_de_fond Numeric(6) NOT NULL primary key ,
+alt VARCHAR(40),
+url VARCHAR(100)
 ) ;
-CREATE TABLE Commandes (
-cno Numeric(6) NOT NULL primary key ,
-fno Numeric(6) NOT NULL ,
-pno Numeric(6) NOT NULL,
-qute INT(5) NOT NULL ,
-constraint FK1 foreign key (fno) references Fournisseurs(fno),
-constraint FK2 foreign key (pno) references Produits(pno)
+
+
+CREATE TABLE Auteur (
+email_auteur VARCHAR(50) NOT NULL primary key ,
+pseudo VARCHAR(30) NOT NULL,
+mot_de_passe VARCHAR(30) NOT NULL ,
+nom VARCHAR(30) NOT NULL ,
+num_telephone Numeric(10) NOT NULL,
+Description VARCHAR(300) ,
+id_im_de_fond Numeric(6),
+constraint FK1 foreign key (id_im_de_fond) references Image_de_fond(id_im_de_fond)
 ) ;
-INSERT INTO FOURNISSEURS (Fno,Nom,Adresse,Ville) VALUES
-(10,"Dupont",NULL,"Lille"),(15,"Durand",NULL,"Lille"),
-(17,"Lefebvre",NULL,"Lille"),(12,"Jacquet",NULL,"Lyon"),
-(14, "Martin",NULL,"Nice"),(13,"Durand",NULL,"Lyon"),
-(11,"Martin",NULL,"Amiens"),(19,"Maurice",NULL,"Paris"),
-(16,"Dupont",NULL,"Paris");
-INSERT INTO PRODUITS (Pno,Design,Prix,Poids,Couleur) VALUES
-(102,"fauteuil",1500,9,"rouge"),(103,"bureau",3500,30,"vert"),
-(101,"fauteuil",2000,7,"gris"),(105,"armoire", 2500,35,"rouge"),
-(104,"bureau",4000,40,"gris"),(107,"caisson",1000,12,"jaune"),
-(106,"caisson",1000,12,"gris"),(108,"classeur",1500,20,"bleu");
-INSERT INTO COMMANDES VALUES
-(1001,17,103,10),(1003,15,103,2),(1005,17,102,1),
-(1007,15,108,1),(1011,19,107,12),(1013,13,107,5),
-(1017,19,105,3),(1019,14,103,10),(1023,10,102,8),
-(1029,17,108,15);
+
+INSERT INTO Image_de_fond (id_im_de_fond,alt,url) VALUES
+(145201,"Image de Paris","Paris.png");
+
