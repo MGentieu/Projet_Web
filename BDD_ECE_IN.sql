62a7f4499aacf57ba7322cb9ad2f6da49fdb15f3
MGentieu <mgentieu02@gmail.com>
2023-11-30 18:21:34 +0100

    Mise à jour partielle de la BDD

--- BDD_ECE_IN.sql
+++ BDD_ECE_IN.sql
@@ -4,6 +4,11 @@ USE ECE_IN ;
 DROP TABLE IF EXISTS Auteur ;
 DROP TABLE IF EXISTS Administrateur ;
 DROP TABLE IF EXISTS Image_de_fond ;
+DROP TABLE IF EXISTS Conversation ;
+DROP TABLE IF EXISTS Conversation ;
+DROP TABLE IF EXISTS Participation ;
+DROP TABLE IF EXISTS Message ;
+DROP TABLE IF EXISTS Amitie ;
 
 CREATE TABLE Administrateur (
 email_admin VARCHAR(50) NOT NULL primary key ,
@@ -29,6 +34,41 @@ Description VARCHAR(300) ,
 id_im_de_fond Numeric(6),
 constraint FK1 foreign key (id_im_de_fond) references Image_de_fond(id_im_de_fond)
 ) ;
+
+CREATE TABLE Conversation (
+id_conv Numeric(6) NOT NULL primary key ,
+date_creation DATE NOT NULL,
+nom_conv VARCHAR(40) NOT NULL
+) ;
+
+CREATE TABLE Participation (
+id_conv Numeric(6) NOT NULL,
+email_auteur VARCHAR(50) NOT NULL,
+PRIMARY KEY (id_conv, email_auteur),
+constraint FK1 foreign key (id_conv) references Conversation(id_conv),
+constraint FK2 foreign key (email_auteur) references Auteur(email_auteur)
+) ;
+
+CREATE TABLE Message (
+id_conv Numeric(6) NOT NULL,
+email_auteur VARCHAR(50) NOT NULL,
+numero Numeric(6) NOT NULL,
+date_envoi DATE NOT NULL,
+heure_envoi Time NOT NULL,
+Contenu VARCHAR(200),
+PRIMARY KEY (id_conv, email_auteur, numero),
+constraint FK1 foreign key (id_conv) references Conversation(id_conv),
+constraint FK2 foreign key (email_auteur) references Auteur(email_auteur)
+) ;
+
+CREATE TABLE Amitie (
+email_ami_1 VARCHAR(50) NOT NULL,
+email_ami_2 VARCHAR(50) NOT NULL,
+PRIMARY KEY (email_ami_1, email_ami_2),
+constraint FK1 foreign key (email_ami_1) references Auteur(email_auteur),
+constraint FK2 foreign key (email_ami_2) references Auteur(email_auteur)
+) ;
+
 
 INSERT INTO Image_de_fond (id_im_de_fond,alt,url) VALUES
 (145201,"Image de Paris","Paris.png");
