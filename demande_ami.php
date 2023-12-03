af056f0f365ef32c621dfa7842c17d18578c5747
MGentieu <mgentieu02@gmail.com>
2023-12-03 21:51:42 +0100

    Finition du formulaire des demandes d'amis
    On vérifie si les emails indiqués existent dans la base de données (table auteur). Si c'est le cas, alors une demande d'ami est envoyée. Il reste encore à blinder et vérifier que la demande d'ami n'a pas déjà été faite.

--- demande_ami.php
+++ demande_ami.php
@@ -4,26 +4,14 @@ $user = 'root';
 $password = ''; //To be completed if you have set a password to root
  //To be completed to connect to a database. The database must exist.
 $port = NULL; //Default must be NULL to use default port
-$verif=false;
-/*$mysqli2=  new mysqli('127.0.0.1', $user, $password, '',$port);
-
-
-if ($mysqli2->connect_error) {
-    die('Connect Error (' . $mysqli2->connect_errno . ') '
-            . $mysqli2->connect_error);
-}
-else{
-    $mysqli2->query("DROP DATABASE IF EXISTS TEST_ECE_IN");
-    $mysqli2->query("CREATE DATABASE TEST_ECE_IN");
-    $mysqli2->query("USE ECE_IN") ;
-}
-$mysqli2->close();
-*/
-
+$valid_form=false;
+$verif1=false;
+$verif2=false;
+$message="";
 $database = 'ECE_IN';
 $mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
 
-if ($mysqli->connect_error||$verif) {
+if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 }
@@ -32,52 +20,57 @@ else{
     echo '<p>Server '.$mysqli->server_info.'</p>';
     echo '<p>Initial charset: '.$mysqli->character_set_name().'</p>';
 
-    /*
-    $mysqli->query("DROP TABLE IF EXISTS Demande_ami") ;
-    $mysqli->query("CREATE TABLE Demande_ami (
-    email_ami_1 VARCHAR(50) NOT NULL,
-    email_ami_2 VARCHAR(50) NOT NULL,
-    PRIMARY KEY (email_ami_1, email_ami_2),
-    constraint FK1 foreign key (email_ami_1) references Auteur(email_auteur),
-    constraint FK2 foreign key (email_ami_2) references Auteur(email_auteur)
-    )");
-    */
-    $result=$mysqli->query("select * from demande_ami where email_ami_1='$email1' and email_ami_2='$email2'");
-    $mysqli->query("INSERT INTO Demande_ami VALUES ('hgentieu97@gmail.com','ericdampierre@gmail.com')");
-
-    //$mysqli->query("DROP TABLE IF EXISTS test1");
-    //$mysqli->query("DROP DATABASE IF EXISTS TEST_ECE_IN");
-    //$mysqli->query("CREATE DATABASE TEST_ECE_IN");
-    //$mysqli->query("USE ECE_IN") ;
-    //$mysqli->query("CREATE TABLE test_table_requet_php(id INT)");
-    //$mysqli->query("DROP TABLE IF EXISTS Administrateur");
-    /*
-    $mysqli->query("CREATE TABLE Administrateur (
-    email_admin VARCHAR(50) NOT NULL primary key ,
-    mot_de_passe VARCHAR(30) NOT NULL ,
-    nom VARCHAR(30) NOT NULL ,
-    prenom VARCHAR(30) NOT NULL ,
-    num_telephone Numeric(10) NOT NULL
-    ) ");
-
-    $mysqli->query("INSERT INTO Administrateur (email_admin,mot_de_passe,nom,prenom,num_telephone)
-    VALUES('mgentieu02@gmail.com','volcan1','Gentieu','Martin',0695973078)");
-
-    //$mysqli->query("INSERT INTO Administrateur (email_admin, mot_de_passe, nom, prenom, num_telephone) VALUES('mgentieu02@gmail.com', 'volcan1', 'Gentieu', 'Martin', 0695973078)");
-    */
-
-    /*
-    CREATE TABLE Administrateur (
-    email_admin VARCHAR(50) NOT NULL primary key ,
-    mot_de_passe VARCHAR(30) NOT NULL ,
-    nom VARCHAR(30) NOT NULL ,
-    prenom VARCHAR(30) NOT NULL ,
-    num_telephone Numeric(10) NOT NULL
-    ) ;
-    */
+    $email1=isset($_POST["e1"])? $_POST["e1"] : ""; 
+    $email2=isset($_POST["e2"])? $_POST["e2"] : ""; 
+     if($email1==""||$email2==""){
+        $valid_form=false;
+        $message.="Un des champs requis est vide.<br>";
+        
+    }
+    else{
+        $valid_form=true;   
+    }
+    if($email1==""){
+        $message.="Le 1er email n'est pas indiqué.<br>";
+    }
+    if($email2==""){
+        $message.="Le 2nd email n'est pas indiqué.<br>";
+    }
+    if(!$valid_form){
+        echo $message;
+    }
+    else{
+        $user1=$mysqli->query("select email_auteur from auteur where email_auteur='$email1'");
+        $user2=$mysqli->query("select email_auteur from auteur where email_auteur='$email2'");
+        $verif1=($user1->num_rows > 0)?true:false;
+        $verif2=($user2->num_rows > 0)?true:false;
+        
+        if($verif1&&$verif2){
+            while($row = $user1->fetch_assoc()) {
+                $message.= "e1 " . $row["email_auteur"]. "<br>";
+            }
+            while($row = $user2->fetch_assoc()) {
+                $message.= "e2 " . $row["email_auteur"]. "<br>";
+            }
+            $mysqli->query("INSERT INTO Demande_ami VALUES ('$email1','$email2')");
+            $message.="La demande d'ami a bien été faite.<br>";
+            
+        }
+        if(!$verif1){
+            $message.="Email du 1er auteur non trouvé dans la BDD ou erreur dans la requete SQL.<br>";
+        }
+        if(!$verif2){
+            $message.="Email du 2nd auteur non trouvé dans la BDD ou erreur dans la requete SQL.<br>";
+        }
     
 
-    echo "<p>La table a bien été créée.</p>";
+        //$mysqli->query("INSERT INTO Administrateur (email_admin, mot_de_passe, nom, prenom, num_telephone) VALUES('mgentieu02@gmail.com', 'volcan1', 'Gentieu', 'Martin', 0695973078)");
+
+        
+        echo $message;
+    }
+    
+    
 }
 
 $mysqli->close();
