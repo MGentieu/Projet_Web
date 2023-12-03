145389a3740f427800c696fdcfc48c2ffa2f7aea
MGentieu <mgentieu02@gmail.com>
2023-12-03 22:12:19 +0100

    Blindage presque final de la demande d'ami
    Il ne devrait maintenant plus y avoir de conflits SQL avec ce blindage.

--- demande_ami.php
+++ demande_ami.php
@@ -5,8 +5,13 @@ $password = ''; //To be completed if you have set a password to root
  //To be completed to connect to a database. The database must exist.
 $port = NULL; //Default must be NULL to use default port
 $valid_form=false;
-$verif1=false;
-$verif2=false;
+
+$verif1=false; //Email 1 existe dans auteur ?
+$verif2=false; //Email 2 existe dans auteur ?
+$verif3=false; //Demande d'ami existe déjà ?
+$verif4=false; //Les 2 emails sont-ils identiques ?
+$verif5=false; //Les 2 utilisateurs sont-ils déjà amis ?
+
 $message="";
 $database = 'ECE_IN';
 $mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
@@ -36,16 +41,20 @@ else{
     if($email2==""){
         $message.="Le 2nd email n'est pas indiqué.<br>";
     }
-    if(!$valid_form){
-        echo $message;
-    }
-    else{
+    
+    if($valid_form){
         $user1=$mysqli->query("select email_auteur from auteur where email_auteur='$email1'");
         $user2=$mysqli->query("select email_auteur from auteur where email_auteur='$email2'");
+        $demande=$mysqli->query("select * from demande_ami where email_ami_1='$email1' and email_ami_2='$email2' or email_ami_2='$email1' and email_ami_1='$email2'");
+        $amitie=$mysqli->query("select * from amitie where email_ami_1='$email1' and email_ami_2='$email2' or email_ami_2='$email1' and email_ami_1='$email2'");
+        
         $verif1=($user1->num_rows > 0)?true:false;
         $verif2=($user2->num_rows > 0)?true:false;
-        
-        if($verif1&&$verif2){
+        $verif3=($demande->num_rows > 0)?false:true;
+        $verif4=($email1==$email2)?false:true;
+        $verif5=($amitie->num_rows > 0)?false:true;
+
+        if($verif1&&$verif2&&$verif3&&$verif4&&$verif5){
             while($row = $user1->fetch_assoc()) {
                 $message.= "e1 " . $row["email_auteur"]. "<br>";
             }
@@ -62,15 +71,25 @@ else{
         if(!$verif2){
             $message.="Email du 2nd auteur non trouvé dans la BDD ou erreur dans la requete SQL.<br>";
         }
+        if($verif1&&$verif2&&!$verif3){
+            $message.="Les 2 emails existent mais la demande d'ami a déjà été faite. Attendez qu'elle soit acceptée.<br>";
+        }
+        if($verif1&&$verif2&&!$verif4){
+            $message.="Les 2 emails sont identiques.<br>";
+        }
+        if($verif1&&$verif2&&!$verif5){
+            $message.="Les 2 emails existent, mais les utilisateurs correspondants sont déjà amis.<br>";
+        }
+    
     
 
         //$mysqli->query("INSERT INTO Administrateur (email_admin, mot_de_passe, nom, prenom, num_telephone) VALUES('mgentieu02@gmail.com', 'volcan1', 'Gentieu', 'Martin', 0695973078)");
 
         
-        echo $message;
+        
     }
     
-    
+    echo $message;
 }
 
 $mysqli->close();
