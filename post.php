059b45ef7df89b6c6602e12266bc720f3a8d9310
MGentieu <mgentieu02@gmail.com>
2023-12-07 11:58:57 +0100

    Tests Martin

--- post.php
+++ post.php
@@ -1,10 +1,11 @@
 <?php 
 session_start(); 
 if(isset($_SESSION['name'])){ 
+	$nom_log="\log.html";
 	$text = $_POST['text']; 
 	$text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>"; 
 	// file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX); 
-	$myfile = fopen(__DIR__ . "/log.html", "a") or die("Impossible d'ouvrir le fichier " . __DIR__ . "/log.html"); 
+	$myfile = fopen(__DIR__ . $nom_log, "a") or die("Impossible d'ouvrir le fichier " . __DIR__ . "/log.html"); 
 	fwrite($myfile, $text_message); 
 	fclose($myfile); 
 } 
