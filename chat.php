d9ca743ffe9669c1e6669c25cf59d7d0f81bdfb3
MGentieu <mgentieu02@gmail.com>
2023-12-11 19:33:10 +0100

    Finition de la messagerie.

--- chat.php
+++ chat.php
@@ -1,26 +1,29 @@
 <?php 
 session_start(); 
+//$url="#";
 if (isset($_GET['logout'])){ 
 //Message de sortie simple 
-	$logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>" . $_SESSION['name'] . "</b> a quitté la session de chat.</span><br></div>"; 
-	$myfile = fopen(__DIR__ . "/log.html", "a") or die("Impossible d'ouvrir le fichier!" . __DIR__ . "/log.html"); 
+	$logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>" . $_SESSION['ep'] . "</b> a quitté la session de chat.</span><br></div>"; 
+	$myfile = fopen($_SESSION['filepath'], "a") or die("Impossible d'ouvrir le fichier " . $_SESSION['filepath']); 
 	fwrite($myfile, $logout_message); 
 	fclose($myfile); 
-	session_destroy(); 
+	//session_destroy(); 
 	sleep(1); 
 	header("Location: messagerie.php"); 
 	//Rediriger l'utilisateur 
 } 
 
+/*
 if (isset($_POST['enter'])){ 
 	if($_POST['name'] != ""){ 
-		$_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name'])); 
+		$_SESSION['ep'] = stripslashes(htmlspecialchars($_POST['ep'])); 
 		//$_SESSION['name'] = "Yolo";
 	} 
 	else{ 
 		echo '<span class="error">Veuillez saisir votre nom</span>'; 
 	} 
 } 
+*/
 
 function loginForm() { 
 	echo '<div id="loginform"> <p>Veuillez saisir votre nom pour continuer!</p> <form action="chat.php" method="post"> <label for="name">Nom: </label> <input type="text" name="name" id="name" /> <input type="submit" name="enter" id="enter" value="Soumettre" /> </form> </div>'; 
@@ -38,21 +41,22 @@ function loginForm() {
 <body> 
 
 	<?php 
-	if (!isset($_SESSION['name'])){ 
+	if (!isset($_SESSION['ep'])){ 
 		loginForm(); 
 	} 
 
 	else { 
+		//$url=$_SESSION['filepath'];
 		?> 
 		<div id="wrapper"> 
 			<div id="menu">
-				<p class="welcome">Bienvenue, <b><?php echo $_SESSION['name']; ?></b></p> 
+				<p class="welcome">Bienvenue, <b><?php echo $_SESSION['ep']; ?></b></p> 
 				<p class="logout"><a id="exit" href="#">Quitter la conversation</a></p> 
 			</div> 
 			<div id="chatbox"> 
 				<?php 
-				if(file_exists("log.html") && filesize("log.html") > 0){ 
-					$contents = file_get_contents("log.html"); 
+				if(file_exists($_SESSION['filepath']) && filesize($_SESSION['filepath']) > 0){ 
+					$contents = file_get_contents($_SESSION['filepath']); 
 					echo $contents; 
 				} 
 				?> 
@@ -81,7 +85,8 @@ function loginForm() {
 				var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; 
 				//Hauteur de défilement avant la requête 
 				$.ajax({ 
-					url: "log.html", 
+					url: '<?php echo $_SESSION['namefile']; ?>', 
+					//url: '1.html',
 					cache: false, 
 					success: function (html) { 
 						$("#chatbox").html(html); 
--- post.php
+++ post.php
@@ -1,11 +1,13 @@
 <?php 
 session_start(); 
-if(isset($_SESSION['name'])){ 
-	$nom_log="\log.html";
+if(isset($_SESSION['ep'])){ 
+	//$nom_log="\log.html";
+	date_default_timezone_set('Europe/Paris');
 	$text = $_POST['text']; 
-	$text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>"; 
+	$text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['ep']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>"; 
 	// file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX); 
-	$myfile = fopen(__DIR__ . $nom_log, "a") or die("Impossible d'ouvrir le fichier " . __DIR__ . "/log.html"); 
+	$filepath=$_SESSION['filepath'];
+	$myfile = fopen($filepath, "a") or die("Impossible d'ouvrir le fichier " . $filepath); 
 	fwrite($myfile, $text_message); 
 	fclose($myfile); 
 } 
