e75f789c8678c530df8bd90da6d8a388e3a78d41
Danae08 <danae.collard@edu.ece.fr>
2023-12-14 13:51:04 +0100

    ?

--- monreseau.php
+++ monreseau.php
@@ -28,11 +28,6 @@ if(!isset($_SESSION['ep'])){
 	<title>Mon réseau</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
     <link href="ecein.css" rel="stylesheet" type="text/css" />
-    <!-- Bibliothèque jQuery --> 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js">
-        
-    </script> <!-- Dernier JavaScript compilé --> 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script type="text/javascript">
@@ -50,9 +45,9 @@ if(!isset($_SESSION['ep'])){
     <div class="wrapper">
 
 
-        <div class="gauche" >
-            <h2>ECE In: Social Media Professionel <br> 
-                 de l'ECE Paris</h2>
+    <div class="gauche" >
+        <h2>ECE In: Social Media Professionel <br> 
+                de l'ECE Paris</h2>
         </div>
 
         <div class="droite">
@@ -83,63 +78,19 @@ if(!isset($_SESSION['ep'])){
         
 
         <div class="leftcolumn">
-        <div id="myCarousel" class="carousel slide" data-ride="carousel"> 
-            <!-- Indicators --> 
-            <ol class="carousel-indicators"> 
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li> 
-                <li data-target="#myCarousel" data-slide-to="1"></li> 
-                <li data-target="#myCarousel" data-slide-to="2"></li> 
-                <li data-target="#myCarousel" data-slide-to="3"></li> 
-            </ol> 
 
-            <!-- Wrapper pour les images --> 
-            <div class="carousel-inner"> 
-                <div class="item active"> 
-                    <img src="images/france1.jpg" alt="Paris" style="width:100px; height: 100px;"> 
-                    <div class="carousel-caption"> 
-                        <h3>Paris</h3> 
-                        <p>La ville lumière!</p> 
-                    </div> 
-                </div> 
-                <div class="item">
-                    <img src="images/france2.jpg" style="width:100px; height:100px">
-                    <div class="carousel-caption">
-                        <h3>Berlin</h3>
-                        <p>La ville grise</p>       
-                    </div>
-                </div>
-
-                <div class="item">
-                    <img src="images/france3.jpg" style="width:100px; height:100px">
-                    <div class="carousel-caption">
-                        <h3>Rome</h3>
-                        <p>La ville romaine</p>     
-                    </div>
-                </div>
-
-                <div class="item">
-                    <img src="images/france4.jpg" style="width:100px; height:100px">
-                    <div class="carousel-caption">
-                        <h3>Prague</h3>
-                        <p>La ville des racistes</p>        
-                    </div>
-                </div>
-            </div>
             
     	</div>
-        </div>
         
 
-        <div class="rightcolumn" style="overflow:scroll;">
+        <div class="rightcolumn">
             <p>ECE In est une platforme de réseau social...
                 <br>
             </p>
         </div>
         <div class="rightestcolumn">
             
-           <p class="logout" style="text-align: center;"><br>
-                <a id="exit" href="#" style="color: #FFF;"><button type="button" class="btn btn-primary" style="width:180px;background-color: #E74C3C; font-size: 2em;">Quitter</button></a>
-            </p>
+            <p class="logout"><a id="exit" href="#" class="action-button animate red">Quitter</a></p>
         </div>
 
         <div id="footer">
