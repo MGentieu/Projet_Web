f56c384d550f664f13d40ff1b22052d185ed3fa0
Danae08 <danae.collard@edu.ece.fr>
2023-12-17 19:56:34 +0100

    demande ami

--- demande_ami.php
+++ demande_ami.php
@@ -1,8 +1,8 @@
 <?php
-
+session_start();
 $user = 'root';
-$password = ''; //To be completed if you have set a password to root
- //To be completed to connect to a database. The database must exist.
+$password = $_SESSION['mdp_bdd'];
+//To be completed to connect to a database. The database must exist.
 $port = NULL; //Default must be NULL to use default port
 $valid_form=false;
 
@@ -26,74 +26,51 @@ else{
     echo '<p>Initial charset: '.$mysqli->character_set_name().'</p>';
 
     $email1=isset($_POST["e1"])? $_POST["e1"] : ""; 
-    $email2=isset($_POST["e2"])? $_POST["e2"] : ""; 
-     if($email1==""||$email2==""){
+
+     if($email1==""){
         $valid_form=false;
-        $message.="Un des champs requis est vide.<br>";
+        $message.="Le champ requis est vide.<br>";
         
     }
     else{
         $valid_form=true;   
     }
-    if($email1==""){
-        $message.="Le 1er email n'est pas indiqué.<br>";
-    }
-    if($email2==""){
-        $message.="Le 2nd email n'est pas indiqué.<br>";
-    }
     
     if($valid_form){
         $user1=$mysqli->query("select email_auteur from auteur where email_auteur='$email1'");
-        $user2=$mysqli->query("select email_auteur from auteur where email_auteur='$email2'");
-        $demande=$mysqli->query("select * from demande_ami where email_ami_1='$email1' and email_ami_2='$email2' or email_ami_2='$email1' and email_ami_1='$email2'");
-        $amitie=$mysqli->query("select * from amitie where email_ami_1='$email1' and email_ami_2='$email2' or email_ami_2='$email1' and email_ami_1='$email2'");
+        $demande=$mysqli->query("select * from demande_ami where email_ami_1='$email1'");
+        $amitie=$mysqli->query("select * from amitie where email_ami_1='$email1'");
         
         $verif1=($user1->num_rows > 0)?true:false;
-        $verif2=($user2->num_rows > 0)?true:false;
         $verif3=($demande->num_rows > 0)?false:true;
         //$verif4=($email1==$email2)?false:true;
 
         //Equivalent
-        if($email1==$email2){
-            $verif4=false;
-        }
-        else{
-            $verif4=true;
-        }
+        
         $verif5=($amitie->num_rows > 0)?false:true;
 
-        if($verif1&&$verif2&&$verif3&&$verif4&&$verif5){
+        if($verif1&&$verif3&&$verif5){
             while($row = $user1->fetch_assoc()) {
                 $message.= "e1 " . $row["email_auteur"]. "<br>";
             }
-            while($row = $user2->fetch_assoc()) {
-                $message.= "e2 " . $row["email_auteur"]. "<br>";
-            }
-            $mysqli->query("INSERT INTO Demande_ami VALUES ('$email1','$email2')");
+            
+            $mysqli->query("INSERT INTO Demande_ami VALUES ('$email1')");
             $message.="La demande d'ami a bien été faite.<br>";
             
         }
         if(!$verif1){
             $message.="Email du 1er auteur non trouvé dans la BDD ou erreur dans la requete SQL.<br>";
         }
-        if(!$verif2){
-            $message.="Email du 2nd auteur non trouvé dans la BDD ou erreur dans la requete SQL.<br>";
+        if($verif1&&!$verif3){
+            $message.="L'email existe mais la demande d'ami a déjà été faite. Attendez qu'elle soit acceptée.<br>";
         }
-        if($verif1&&$verif2&&!$verif3){
-            $message.="Les 2 emails existent mais la demande d'ami a déjà été faite. Attendez qu'elle soit acceptée.<br>";
+        if(!$verif5){
+            $message.="L'email existe, mais les utilisateurs correspondants sont déjà amis.<br>";
         }
-        if($verif1&&$verif2&&!$verif4){
-            $message.="Les 2 emails sont identiques.<br>";
-        }
-        if($verif1&&$verif2&&!$verif5){
-            $message.="Les 2 emails existent, mais les utilisateurs correspondants sont déjà amis.<br>";
-        }
-    
     
 
         //$mysqli->query("INSERT INTO Administrateur (email_admin, mot_de_passe, nom, prenom, num_telephone) VALUES('mgentieu02@gmail.com', 'volcan1', 'Gentieu', 'Martin', 0695973078)");
 
-        
         
     }
     
